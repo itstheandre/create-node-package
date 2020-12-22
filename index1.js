fee43ba9088d8d24ae7089d066a8bae0e0771736
Andre <itstheandre@gmail.com>
2020-12-22 19:33:28 +0000

    me

---
+++ index1.js
@@ -0,0 +1,11 @@
+// "husky": {
+//    "hooks": {
+//      "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
+//      "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true"
+//    }
+//  },
+//  "config": {
+//    "commitizen": {
+//      "path": "./node_modules/cz-conventional-changelog"
+//    }
+//  },
